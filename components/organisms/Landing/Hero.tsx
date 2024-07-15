800f6e25836114f5d09c9f12ece35f945de03ef2
jollygrin <JollyGrin@protonmail.com>
2024-07-15 09:43:02 +0200

    refactor hero

---
+++ components/organisms/Landing/Hero.tsx
@@ -0,0 +1,49 @@
+import { Button, Box, Grid, Text, VStack } from "@chakra-ui/react";
+import { LuArrowUpRight as IconArrow } from "react-icons/lu";
+
+export const LandingHero = () => {
+  return (
+    <Grid
+      minH="80vh"
+      placeItems="center"
+      backgroundImage="/houses2.svg"
+      backgroundPosition="bottom"
+      backgroundSize="unset"
+      backgroundRepeat="repeat"
+      mx="-2rem"
+      position="relative"
+      p="2rem"
+    >
+      <Box
+        w="100%"
+        h="100px"
+        bg="linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(255,248,243,0.75) 50%, rgba(255,248,243,1) 100%)"
+        position="absolute"
+        top="0"
+      />
+      <VStack alignItems="start" gap="2rem" maxW="600px">
+        <Text
+          fontFamily="header"
+          fontWeight={500}
+          fontSize="9rem"
+          lineHeight="8rem"
+        >
+          Don&apos;t trust us,
+          <br /> trust our clients
+        </Text>
+
+        <Text fontSize="2.5rem">
+          Over 300 listings, apartments, lots and more available now
+        </Text>
+        <Button rightIcon={<IconArrow fontSize="3rem" />}>Get in touch</Button>
+      </VStack>
+      <Box
+        w="100%"
+        h="50px"
+        bg="linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(255,248,243,0.75) 50%, rgba(255,248,243,1) 100%)"
+        position="absolute"
+        bottom="0"
+      />
+    </Grid>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import { Nav } from "@/components/molecules/Nav";
+import { LandingHero } from "@/components/organisms/Landing/Hero";
 import { Button, Box, Grid, Text, VStack } from "@chakra-ui/react";
 import Head from "next/head";
-
-import { LuArrowUpRight as IconArrow } from "react-icons/lu";
 
 export default function Home() {
   return (
@@ -16,41 +15,7 @@ export default function Home() {
       <main>
         <Box p="2rem">
           <Nav />
-          <Grid
-            minH="50vh"
-            placeItems="center"
-            backgroundImage="/houses2.svg"
-            backgroundPosition="bottom"
-            backgroundSize="unset"
-            backgroundRepeat="repeat"
-            mx="-2rem"
-            position="relative"
-          >
-            <Box
-              w="100%"
-              h="100px"
-              bg="linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(255,248,243,0.75) 50%, rgba(255,248,243,1) 100%)"
-              position="absolute"
-              top="0"
-            />
-            <VStack alignItems="start" gap="2rem">
-              <Text
-                fontFamily="header"
-                fontWeight={500}
-                fontSize="13rem"
-                lineHeight="10rem"
-              >
-                Don&apos;t trust us,
-                <br /> trust our clients
-              </Text>
-              <Text fontSize="2.5rem">
-                Over 300 listings, apartments, lots and more available now
-              </Text>
-              <Button rightIcon={<IconArrow fontSize="3rem" />}>
-                Get in touch
-              </Button>
-            </VStack>
-          </Grid>
+          <LandingHero />
         </Box>
       </main>
     </>
