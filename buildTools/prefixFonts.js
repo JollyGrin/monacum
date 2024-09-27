b5b0f2d09ae257f834a3d1e4632fdbff2d211fd3
jollygrin <JollyGrin@protonmail.com>
2024-09-28 00:19:41 +0200

    fix build

---
+++ buildTools/prefixFonts.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+// Set the base URL for your CSS files
+const cssUrl = "/static/css/";
+// Set the directory where your HTML files are located
+const htmlDir = path.join(__dirname, `../../.next${cssUrl}`);
+
+// Set the base URL for your fonts
+// const fontsUrl = "/unbrewed-p2p/fonts/";
+const fontsUrl = "/fonts/";
+
+// Loop through all HTML files in the directory
+fs.readdirSync(htmlDir).forEach((file) => {
+  console.info("FILE:", file);
+  if (file.endsWith(".css")) {
+    const filePath = path.join(htmlDir, file);
+    let html = fs.readFileSync(filePath, "utf8");
+
+    // Find the CSS file name
+    const cssFile = fs
+      .readdirSync(".next/static/css")
+      .find((file) => file.endsWith(".css"));
+
+    console.info("FOUND:", cssFile);
+    const fontPatternRegex = /src:\s*url\(["']?\/fonts\//;
+
+    html = html.replace(fontPatternRegex, `src: url(${fontsUrl}`);
+    html = html.replace(fontPatternRegex, `src: url(${fontsUrl}`);
+    html = html.replace(fontPatternRegex, `src: url(${fontsUrl}`);
+
+    // Write the updated HTML file back to disk
+    fs.writeFileSync(filePath, html);
+  }
+});
--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const pathPrefix = "";
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
+  assetPrefix: pathPrefix,
+  env: {
+    pathPrefix,
+  },
 };
 
 module.exports = nextConfig;
